02b358a9cc22c167c838e14bbfc80a34de080258
Adam Fučík <155400992+bababooex@users.noreply.github.com>
2026-02-10 22:08:53 +0100

    Add files via upload

---
+++ inc/av2026.h
@@ -0,0 +1,37 @@
+#ifndef AV2026_H
+#define AV2026_H
+
+#include "swi2c.h"
+#include "stm8s_delay.h"
+// AV2026 default address and xtal frequency in kHz
+#define AV2026_ADDR   (0x60 << 1)
+#define AV2026_XTAL   27000
+// offset tuner to get IQ output as IF for easy work
+#define FREQ_OFFSET 44
+// very high bandwidth to avoid filtering issue at IF (doesnt matter, pushing BW to its limits)
+#define AV2026_tune(frec) AV2026_setFrequency(frec, 100000)
+//init registers for sat tuner - total 42
+static const uint8_t initRegs[] = {
+        0x38,0x00,0x00,0x50,0x1F,0xA3,0xFD,0x58,
+        0x1E,0x82,0x88,0xB4,
+        0xD6,0x40,0x5B,0x6A,
+        0x66,0x40,0x80,0x2B,
+        0x6A,0x50,0x91,0x27,
+        0x8F,0xCC,0x21,0x10,
+        0x80,0xEE,0xF5,0x7F,
+        0x4A,0x9B,0xE0,0xE0,
+        0x36,0x00,0xAB,0x97,
+        0xC5,0xA8
+    };
+
+/**
+ * @brief functions regarding this sat tuner driver
+ * - write registers
+ * - set frequency and symbolrate (bandwidth)
+ */
+
+void AV2026_write_regs(uint8_t regAddr, uint8_t* data, uint8_t length);
+void AV2026_setFrequency(float frec, uint32_t bb_sym);
+
+
+#endif //AV2026_H
---
+++ src/av2026.c
@@ -0,0 +1,130 @@
+#include "av2026.h"
+//helper
+void AV2026_write_regs(uint8_t regAddr, uint8_t* data, uint8_t length)
+{
+    uint8_t buf[16];
+    uint8_t i;
+
+    buf[0] = regAddr;
+
+    for (i = 0; i < length; i++)
+        buf[i + 1] = data[i];
+
+    swi2c_write_array(AV2026_ADDR, buf, length + 1);
+}
+//main logic
+void AV2026_setFrequency(float frec, uint32_t bb_sym)
+{
+    static uint8_t tuner_initial = 0;
+		uint8_t i;
+		uint32_t p1;
+		uint32_t p2;
+		
+		uint32_t freq_khz;
+    uint8_t reg[50];
+    uint8_t reg_start;
+    uint32_t fracN;
+    uint32_t BW;
+    uint32_t BF;
+    uint8_t r;
+    uint8_t auto_scan = 0;
+
+    if (bb_sym == 0 || bb_sym == 45000)
+        auto_scan = 1;
+
+    if (tuner_initial == 0)
+    {
+        for (i = 0; i < 42; i++)
+            reg[i] = initRegs[i];
+
+        reg_start = 0;
+        AV2026_write_regs(reg_start, reg, 12);
+        delay_ms(1);
+
+        reg_start = 13;
+        AV2026_write_regs(reg_start, reg + 13, 12);
+
+        reg_start = 25;
+        AV2026_write_regs(reg_start, reg + 25, 11);
+
+        reg_start = 36;
+        AV2026_write_regs(reg_start, reg + 36, 6);
+
+        delay_ms(1);
+
+        reg_start = 12;
+				AV2026_write_regs(reg_start, reg + 12, 1);
+
+        delay_ms(100);
+				
+        reg_start = 0;
+        AV2026_write_regs(reg_start, reg, 12);
+        delay_ms(1);
+
+        reg_start = 13;
+        AV2026_write_regs(reg_start, reg + 13, 12);
+
+        reg_start = 25;
+        AV2026_write_regs(reg_start, reg + 25, 11);
+
+        reg_start = 36;
+        AV2026_write_regs(reg_start, reg + 36, 6);
+
+        delay_ms(1);
+
+        reg_start = 12;
+        AV2026_write_regs(reg_start, reg + 12, 1);
+
+        delay_ms(4);
+
+        tuner_initial = 1;
+    }
+		
+		frec=frec-FREQ_OFFSET;//calculate IF offset to act as IF, not as zero IF
+    freq_khz = (uint32_t)(frec * 1000);//convert float to khz
+    fracN = (freq_khz + AV2026_XTAL / 2) / AV2026_XTAL;
+    if (fracN > 0xFF) fracN = 0xFF;
+
+    reg[0] = fracN & 0xFF;
+		//handle too large number with two 32 bit uints
+		p1 = freq_khz / AV2026_XTAL;
+		p2 = freq_khz % AV2026_XTAL;
+
+		fracN = (p1 << 17) + ((p2 << 17) / AV2026_XTAL);
+    fracN = fracN & 0x1FFFF;
+		
+    reg[1] = ((fracN >> 9) & 0xFF);
+    reg[2] = ((fracN >> 1) & 0xFF);
+    reg[3] = (((fracN << 7) & 0x80) | 0x50);
+
+    if (auto_scan)
+    {
+        reg[5] = 0xA3;
+    }
+    else
+    {    
+        BW = bb_sym * 135 / 200;
+
+        if (bb_sym < 6500)
+            BW = BW + 6000;
+
+        BW = BW + 2000;
+        BW = BW * 108 / 100;
+
+        if (BW < 4000)  BW = 4000;
+        if (BW > 40000) BW = 40000;
+
+        BF = (BW * 127 + 21100/2) / (21100);
+        reg[5] = (uint8_t)BF;
+    }
+    AV2026_write_regs(0, reg, 4);
+    AV2026_write_regs(5, reg + 5, 1);
+    if (!auto_scan)
+    {
+        reg[37] = 0x06;
+        AV2026_write_regs(37, reg + 37, 1);
+
+        reg[12] = 0x96;
+        AV2026_write_regs(12, reg + 12, 1);
+    }
+}
